55c4d265f12d333265a0ffa3df76d5b631c87ad3
John Williams <johndwill@gmail.com>
2014-12-17 17:23:28 -0500

    Add some style

--- letter.rb
+++ letter.rb
@@ -12,16 +12,40 @@ require 'yaml'
 # #####################################################
 get '/' do
 	logger.info request
+	@details = YAML.load_file(File.dirname(__FILE__) + "/details.yaml")
+
 	haml :index
 end
 
 post '/generate' do
 	@name = params[:name]
+	@religion = params[:religion]
 	@details = YAML.load_file(File.dirname(__FILE__) + "/details.yaml")
 	haml :letter
 end
 
 helpers do
+	def religion_selector
+
+		block = "<select name='religion'>"
+		@details[:religion].each do |religion|
+			name = religion[:label]
+			block += "<option value='#{religion[:key]}'>#{religion[:label]}</option>"
+		end
+		block += "</select>"
+		block
+	end
+
+	def greeting
+		@details[:religion].each do |religion|
+			if religion[:key] == @religion
+				str = religion[:greeting]
+				return str.gsub(/NAME/,@name)
+			end
+		end
+		return "General Greeting To You"
+	end
+
 	def cat_antics
 		stories = []
 		indices = (0..4).to_a.shuffle
